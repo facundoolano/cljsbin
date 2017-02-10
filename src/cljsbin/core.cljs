6e476f895422f9e6710673917a633a99e19d39a2
Facundo Olano <facundo.olano@gmail.com>
2017-02-09 22:56:05 -0300

    added some simple routes

--- src/cljsbin/core.cljs
+++ src/cljsbin/core.cljs
@@ -4,7 +4,6 @@
     [cljsbin.middleware :refer [wrap-defaults]]
     [cljsbin.routes :refer [router]]
     [macchiato.server :as http]
-    [macchiato.session.memory :as mem]
     [mount.core :as mount :refer [defstate]]
     [taoensso.timbre :refer-macros [log trace debug info warn error fatal]]))
 
--- src/cljsbin/routes.cljs
+++ src/cljsbin/routes.cljs
@@ -2,17 +2,19 @@
   (:require
     [bidi.bidi :as bidi]
     [hiccups.runtime]
-    [macchiato.util.response :as r])
+    [macchiato.util.response :as r]
+    [camel-snake-kebab.core :refer [->HTTP-Header-Case]])
   (:require-macros
     [hiccups.core :refer [html]]))
 
+;; TODO autogenerate index based on router list
 (defn home [req res raise]
   (-> (html
         [:html
          [:body
           [:h2 "Hello World!"]
           [:p
-           "Your user-agent is: "
+           "Your user-agent is:"
            (str (get-in req [:headers "user-agent"]))]]])
       (r/ok)
       (r/content-type "text/html")
@@ -27,8 +29,44 @@
       (r/content-type "text/html")
       (res)))
 
+(defn ip [req res raise]
+  "Returns Origin IP."
+  (-> {:origin (:remote-addr req)}
+   (r/ok)
+   (r/content-type "application/json")
+   (res)))
+
+(defn user-agent [req res raise]
+  "Returns user-agent."
+  (-> {:user-agent (get-in req [:headers "user-agent"])}
+      (r/ok)
+      (r/content-type "application/json")
+      (res)))
+
+;; FIXME differnt headers: macchiato session,cache control, empty length and type
+(defn clean-headers
+  "Return a sorted map of headers with the proper casing."
+  [req]
+  (->> (:headers req)
+       (map (fn [[k v]] [(->HTTP-Header-Case k) v]))
+       (into (sorted-map))))
+
+(defn headers [req res raise]
+  "Returns header dict."
+  (-> {:headers (clean-headers req)}
+    (r/ok)
+    (r/content-type "application/json")
+    (res)))
+
+(defn get_ [req res raise]
+  "Returns GET data.")
+
 (def routes
-  ["/" {:get home}])
+  ["" {"/" {:get home}
+       "/ip" {:get ip}
+       "/user-agent" {:get user-agent}
+       "/headers" {:get headers}
+       "/get" {:get get_}}])
 
 (defn router [req res raise]
   (if-let [{:keys [handler route-params]} (bidi/match-route* routes (:uri req) req)]
