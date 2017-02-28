285db4b821805466f3e6ec178693ff286cfcf7ad
Facundo Olano <facundo.olano@gmail.com>
2017-02-28 14:12:16 -0300

    rearrange route index

--- src/cljsbin/route_spec.cljc
+++ src/cljsbin/route_spec.cljc
@@ -3,9 +3,11 @@
 (defmacro route-spec
   "Build a route spec usable by bidy (map method to handler) and include
   metadata that can be used to generate a router index."
-  [method sym & {:keys [bidi-tag]}]
+  [method sym & {:keys [bidi-tag no-display]}]
   (if bidi-tag
     `{~method (bidi/tag ~sym ~bidi-tag)
-      :meta (meta (var ~sym))}
+      :meta (meta (var ~sym))
+      :no-display ~no-display}
     `{~method ~sym
-      :meta (meta (var ~sym))}))
+      :meta (meta (var ~sym))
+      :no-display ~no-display}))
--- src/cljsbin/routes.cljs
+++ src/cljsbin/routes.cljs
@@ -12,18 +12,19 @@
 
 (defn render-endpoint
   "Take the route url and spec and render an index entry based on its metadata."
-  [[url {meta_ :meta get-handler :get}]]
-  (let [display-path (if (string? url) url (apply str url))
-        display-url (str display-path (:display-query meta_))
-        params-reducer (fn [path segment]
-                         (str path (get-in meta_ [:href-params segment] segment)))
-        href-path (if (string? url) display-path (reduce params-reducer url))
-        href-url (str href-path (:href-query meta_))]
-    [:li
-     (if get-handler
-       [:a {:data-bare-link "true", :href href-url} [:code display-url]]
-       [:code display-url])
-     (str " " (:doc meta_))]))
+  [[url {meta_ :meta get-handler :get no-display :no-display}]]
+  (if-not no-display
+    (let [display-path (if (string? url) url (apply str url))
+          display-url (str display-path (:display-query meta_))
+          params-reducer (fn [path segment]
+                           (str path (get-in meta_ [:href-params segment] segment)))
+          href-path (if (string? url) display-path (reduce params-reducer url))
+          href-url (str href-path (:href-query meta_))]
+      [:li
+       (if get-handler
+         [:a {:data-bare-link "true", :href href-url} [:code display-url]]
+         [:code display-url])
+       (str " " (:doc meta_))])))
 
 (defn home
   "This page."
@@ -37,7 +38,7 @@
          [:style {:media "all", :type "text/css"} "\n  /* style: man */\n  body#manpage {margin:0}\n  .mp {max-width:100ex;padding:0 9ex 1ex 4ex}\n  .mp p,.mp pre,.mp ul,.mp ol,.mp dl {margin:0 0 20px 0}\n  .mp h2 {margin:10px 0 0 0}\n  .mp > p,.mp > pre,.mp > ul,.mp > ol,.mp > dl {margin-left:8ex}\n  .mp h3 {margin:0 0 0 4ex}\n  .mp dt {margin:0;clear:left}\n  .mp dt.flush {float:left;width:8ex}\n  .mp dd {margin:0 0 0 9ex}\n  .mp h1,.mp h2,.mp h3,.mp h4 {clear:left}\n  .mp pre {margin-bottom:20px}\n  .mp pre+h2,.mp pre+h3 {margin-top:22px}\n  .mp h2+pre,.mp h3+pre {margin-top:5px}\n  .mp img {display:block;margin:auto}\n  .mp h1.man-title {display:none}\n  .mp,.mp code,.mp pre,.mp tt,.mp kbd,.mp samp,.mp h3,.mp h4 {font-family:monospace;font-size:14px;line-height:1.42857142857143}\n  .mp h2 {font-size:16px;line-height:1.25}\n  .mp h1 {font-size:20px;line-height:2}\n  .mp {text-align:justify;background:#fff}\n  .mp,.mp code,.mp pre,.mp pre code,.mp tt,.mp kbd,.mp samp {color:#131211}\n  .mp h1,.mp h2,.mp h3,.mp h4 {color:#030201}\n  .mp u {text-decoration:underline}\n  .mp code,.mp strong,.mp b {font-weight:bold;color:#131211}\n  .mp em,.mp var {font-style:italic;color:#232221;text-decoration:none}\n  .mp a,.mp a:link,.mp a:hover,.mp a code,.mp a pre,.mp a tt,.mp a kbd,.mp a samp {color:#0000ff}\n  .mp b.man-ref {font-weight:normal;color:#434241}\n  .mp pre {padding:0 4ex}\n  .mp pre code {font-weight:normal;color:#434241}\n  .mp h2+pre,h3+pre {padding-left:0}\n  ol.man-decor,ol.man-decor li {margin:3px 0 10px 0;padding:0;float:left;width:33%;list-style-type:none;text-transform:uppercase;color:#999;letter-spacing:1px}\n  ol.man-decor {width:100%}\n  ol.man-decor li.tl {text-align:left}\n  ol.man-decor li.tc {text-align:center;letter-spacing:4px}\n  ol.man-decor li.tr {text-align:right;float:right}\n  "]
          [:style {:media "all", :type "text/css"} "\n  /* style: 80c */\n  .mp {max-width:86ex}\n  ul {list-style: None; margin-left: 1em!important}\n  .man-navigation {left:101ex}\n  "]]
         [:body#manpage
-         [:a {:href "http://github.com/facundoolano/cljsbin"}
+         [:a {:href "https://github.com/facundoolano/cljsbin"}
           [:img {:alt "Fork me on GitHub",
                  :src "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png",
                  :style "position: absolute; top: 0; right: 0; border: 0;"}]]
@@ -62,7 +63,7 @@
           [:pre [:code "HTTP/1.1 418 I'M A TEAPOT\nServer: nginx/0.7.67\nDate: Mon, 13 Jun 2011 04:25:38 GMT\nConnection: close\nx-more-info: http://tools.ietf.org/html/rfc2324\nContent-Length: 135\n"]]
           [:h3#-curl-https-httpbin-org-get-show_env-1 "$ curl https://cljsbin.org/get?show_env=1"]
           [:pre [:code "{\n  \"headers\": {\n    \"Content-Length\": \"\",\n    \"Accept-Language\": \"en-US,en;q=0.8\",\n    \"Accept-Encoding\": \"gzip,deflate,sdch\",\n    \"X-Forwarded-Port\": \"443\",\n    \"X-Forwarded-For\": \"109.60.101.240\",\n    \"Host\": \"httpbin.org\",\n    \"Accept\": \"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\",\n    \"User-Agent\": \"Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.83 Safari/535.11\",\n    \"X-Request-Start\": \"1350053933441\",\n    \"Accept-Charset\": \"ISO-8859-1,utf-8;q=0.7,*;q=0.3\",\n    \"Connection\": \"keep-alive\",\n    \"X-Forwarded-Proto\": \"https\",\n    \"Cookie\": \"_gauges_unique_day=1; _gauges_unique_month=1; _gauges_unique_year=1; _gauges_unique=1; _gauges_unique_hour=1\",\n    \"Content-Type\": \"\"\n  },\n  \"args\": {\n    \"show_env\": \"1\"\n  },\n  \"origin\": \"109.60.101.240\",\n  \"url\": \"http://httpbin.org/get?show_env=1\"\n}\n"]]
-          [:h2#Installing-and-running-from-PyPI "Installing and running from github"]
+          [:h2#Installing-and-running-from-PyPI "Installing and running from GitHub"]
           [:p "You can install httpbin as a library from PyPI and run it as a WSGI app.  For example, using Gunicorn:"]
           [:pre [:code.bash "$ pip install httpbin\n$ gunicorn httpbin:app\n"]]
           [:h2#AUTHOR "AUTHOR"]
@@ -184,34 +185,34 @@
                            "/put" (route-spec :put ep/put)
                            "/patch" (route-spec :patch ep/patch)
                            "/delete" (route-spec :delete ep/delete)
+                           "/forms/post" (route-spec :get form-post)
+                           ["/status/" :status] (route-spec :get ep/status)
+                           "/response-headers" (route-spec :get ep/response-headers)
+                           "/cookies" (route-spec :get ep/cookies)
+                           "/cookies/set" (route-spec :get ep/set-cookies)
+                           "/cookies/delete" (route-spec :get ep/delete-cookies)
+                           "/cache" (route-spec :get ep/cache)
+                           ["/cache/" :n] (route-spec :get ep/cache-seconds)
+                           ["/delay/" :n] (route-spec :get ep/delay_)
+                           ["/redirect/" :n] (route-spec :get redirect)
+                           ["/absolute-redirect/" :n] (route-spec :get absolute-redirect)
+                           "/redirect-to" (route-spec :get redirect-to)
+                           ["/basic-auth/" :user "/" :pass] (route-spec :get ep/basic-auth)
+                           ["/hidden-basic-auth/" :user "/" :pass] (route-spec :get ep/hidden-basic-auth)
+                           ["/digest-auth/" :user "/" :pass] (route-spec :get ep/digest-auth)
+                           ["/links/" :n "/" :index] (route-spec :get links :bidi-tag :links :no-display true)
+                           ["/links/" :n] (route-spec :get links)
                            "/encoding/utf8" (route-spec :get ep/encoding)
                            "/xml" (route-spec :get ep/xml)
                            "/html" (route-spec :get ep/html)
                            "/robots.txt" (route-spec :get ep/robots)
                            "/deny" (route-spec :get ep/deny)
-                           "/cache" (route-spec :get ep/cache)
-                           ["/cache/" :n] (route-spec :get ep/cache-seconds)
-                           ["/status/" :status] (route-spec :get ep/status)
-                           ["/delay/" :n] (route-spec :get ep/delay_)
-                           "/response-headers" (route-spec :get ep/response-headers)
-                           "/cookies" (route-spec :get ep/cookies)
-                           "/cookies/set" (route-spec :get ep/set-cookies)
-                           "/cookies/delete" (route-spec :get ep/delete-cookies)
                            "/image" (route-spec :get ep/image)
                            "/image/png" (route-spec :get ep/image-png)
                            "/image/webp" (route-spec :get ep/image-webp)
                            "/image/svg" (route-spec :get ep/image-svg)
                            "/image/jpeg" (route-spec :get ep/image-jpeg)
-                           ["/basic-auth/" :user "/" :pass] (route-spec :get ep/basic-auth)
-                           ["/hidden-basic-auth/" :user "/" :pass] (route-spec :get ep/hidden-basic-auth)
-                           ["/digest-auth/" :user "/" :pass] (route-spec :get ep/digest-auth)
-                           "/compress" (route-spec :get ep/compress)
-                           "/forms/post" (route-spec :get form-post)
-                           ["/links/" :n "/" :index] (route-spec :get links :bidi-tag :links)
-                           ["/links/" :n] (route-spec :get links)
-                           ["/redirect/" :n] (route-spec :get redirect)
-                           ["/absolute-redirect/" :n] (route-spec :get absolute-redirect)
-                           "/redirect-to" (route-spec :get redirect-to))])
+                           "/compress" (route-spec :get ep/compress))])
 
 (defn router [req res raise]
   (if-let [{:keys [handler route-params]} (bidi/match-route* routes (:uri req) req)]
